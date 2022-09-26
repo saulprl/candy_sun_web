2bb77dfa5cd3e2b65c4cb5df15735ead78ff06fd
Saul <phasecross@hotmail.com>
2022-09-26 13:34:58 -0700

    Added an employee model

---
+++ lib/models/employee.dart
@@ -0,0 +1,19 @@
+class Employee {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String position;
+  final DateTime dob;
+  final DateTime hireDate;
+  final String picture;
+
+  Employee({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.position,
+    required this.dob,
+    required this.hireDate,
+    required this.picture,
+  });
+}
